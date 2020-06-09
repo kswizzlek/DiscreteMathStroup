35f0fb58e6ed7e86459f99395195d88bf14b1228
Kevin Kruger <kkruger@foundationsoft.com>
2020-06-09 12:34:49 -0400

    added comments on recursion fixed format in readme

--- EuclideanAlgorithm/gcd/gcd.py
+++ EuclideanAlgorithm/gcd/gcd.py
@@ -2,6 +2,13 @@ def get_gcd(a, b):
     if(a == 0) :
         return b, 0, 1
     
+    #this is a recursive algorithm
+    #here is an article on recursion
+    #https://www.cs.utah.edu/~germain/PPS/Topics/recursion.html
+    #We need to be carful with recursion in python because of the way python 
+    #executes recursive code. if we go too many layers deep with recursion in python 
+    #we will get a stack overflow error.
+    #Here is an article on this - https://www.geeksforgeeks.org/python-handling-recursion-limit/
     gcd, x1, y1 = get_gcd(b%a, a)
 
     x = y1 - (b//a) * x1
